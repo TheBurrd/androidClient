1761896c56f50e4e8ae7fa5f1e07f98c36c1ec52
Sherman Cheung <shermancheung314@gmail.com>
2015-05-09 18:01:29 -0700

    Edit layout and create secret button

--- app/src/main/java/com/dgaf/happyhour/View/About.java
+++ app/src/main/java/com/dgaf/happyhour/View/About.java
@@ -9,11 +9,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.dgaf.happyhour.R;
 
 /*This is the fragment that our page view loads*/
-public class About extends Fragment {
+public class About extends Fragment implements View.OnClickListener {
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -23,6 +26,10 @@ public class About extends Fragment {
      * number.
      */
 
+    ImageView llamaPic;
+    boolean displayed = false;
+    boolean transparent = true;
+    Button llamaButton;
 
     //section ID acts like ID for the query search as well
     public static About newInstance(int sectionNumber) {
@@ -42,6 +49,32 @@ public class About extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+        llamaButton = (Button) rootView.findViewById(R.id.llamaButton);
+        llamaButton.setOnClickListener(this);
+        llamaButton.setAlpha(0.0f);
+
+        llamaPic = (ImageView) rootView.findViewById(R.id.llamaPic);
+        llamaPic.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.llamaButton:
+                if (!displayed) {
+                    llamaPic.setVisibility(View.VISIBLE);
+                    displayed = true;
+                    llamaButton.setAlpha(0.3f);
+                    transparent = false;
+
+                }
+                else {
+                    llamaPic.setVisibility(View.INVISIBLE);
+                    displayed = false;
+                }
+
+        }
     }
 }
